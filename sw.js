1e4cef99a9e1ba4bbf35ca5b2509f3fb6d18a087
Timur Valeev <t.valeev@omp.ru>
2025-06-06 11:44:35 +0300

    Temprorary disable notifications

--- sw.js
+++ sw.js
@@ -10,11 +10,11 @@ self.addEventListener('activate', event => {
 self.addEventListener('sync', event => {
   if (event.tag === 'test-sync') {
     console.log('[SW] Sync event triggered');
-    event.waitUntil(
-      self.registration.showNotification('✅ Background Sync Fired!', {
-        body: 'Sync completed successfully!',
-        icon: 'icon-512.png'
-      })
-    );
+    //event.waitUntil(
+    //  self.registration.showNotification('✅ Background Sync Fired!', {
+    //    body: 'Sync completed successfully!',
+    //    icon: 'icon-512.png'
+    //  })
+    //);
   }
 });
