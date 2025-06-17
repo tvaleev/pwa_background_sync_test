67a6060d2675b239a07057844569e66b64172cff
Timur Valeev <t.valeev@omp.ru>
2025-06-17 10:03:53 +0300

    Add indicator

--- sw.js
+++ sw.js
@@ -10,11 +10,21 @@ self.addEventListener('activate', event => {
 self.addEventListener('sync', event => {
   if (event.tag === 'test-sync') {
     console.log('[SW] Sync event triggered');
+
     //event.waitUntil(
     //  self.registration.showNotification('✅ Background Sync Fired!', {
     //    body: 'Sync completed successfully!',
     //    icon: 'icon-512.png'
     //  })
     //);
+
+    event.waitUntil(
+      (async () => {
+        const allClients = await self.clients.matchAll({ includeUncontrolled: true });
+        for (const client of allClients) {
+          client.postMessage({ type: 'sync-event', message: 'Sync event has been triggered!' });
+        }
+      })()
+    );
   }
 });
